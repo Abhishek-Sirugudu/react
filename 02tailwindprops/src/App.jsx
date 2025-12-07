cc655c1aeb8520789cb8efd24dc3fdf0008939f1
Abhishek-Sirugudu <siruguduabhishek@gmail.com>
2025-12-07 23:31:10 +0530

    Learned props and components isntalled tailwind

---
+++ 02tailwindprops/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Card from "./components/Card"
+function App() {
+
+  return (
+    <>
+      <h1 className='bg-white text-black rounded-b-lg mb-4'>Hi from Abhishek</h1>
+      <Card name="Abhishek"/>
+      <Card name='Teri behn ki' />
+    </>
+  )
+}
+
+export default App
